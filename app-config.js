cd3149783d38efaa1d691f59314c15a319cf7acd
kth5762 <kth5762@gmail.com>
2026-06-09 16:43:19 +0900

    Add files via upload

---
+++ app-config.js
@@ -0,0 +1,4 @@
+window.RD_OPS_CONFIG = {
+  storageMode: 'google-drive',
+  googleAppsScriptUrl: 'https://script.google.com/macros/s/AKfycbz93CMyYslblWugSkZB68nhsYLZcv7rW6mdwmwBO6dReMYF5ujaE8fkRlBCMMpWwDzzaQ/exec',
+};
