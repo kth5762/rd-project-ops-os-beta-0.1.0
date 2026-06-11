48852bd3467a5752ab5f478b82f90fdb0441f390
kth5762 <kth5762@gmail.com>
2026-06-11 09:22:00 +0900

    Update Google Apps Script URL in app-config.js

--- app-config.js
+++ app-config.js
@@ -1,5 +1,5 @@
 window.RD_OPS_CONFIG = {
   storageMode: 'google-drive',
-  googleAppsScriptUrl: 'https://script.google.com/macros/s/AKfycbw9H-HhZoBIa2dV638vVNsv_8Cz7BBgBwW6iUk8NHLAv8TbUIdYbHgJ7vd2xq17zJW6/exec',
+  googleAppsScriptUrl: 'https://script.google.com/macros/s/AKfycbxHCiMY48BTEGU0pTTuY-lPKn2Ir2BgiUtMbhFaGMtrMkw_2gtY74euuQsxQUQwzZAG/exec',
   openaiModel: 'gpt-5.5-pro',
 };
