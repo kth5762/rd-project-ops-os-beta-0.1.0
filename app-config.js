d24c0ff4e9ef8e3f22de706ecd8dcc7db2255f1d
kth5762 <kth5762@gmail.com>
2026-06-11 09:13:07 +0900

    Update app-config.js

--- app-config.js
+++ app-config.js
@@ -1,5 +1,5 @@
 window.RD_OPS_CONFIG = {
   storageMode: 'google-drive',
-  googleAppsScriptUrl: '',
+  googleAppsScriptUrl: 'https://script.google.com/macros/s/AKfycbw9H-HhZoBIa2dV638vVNsv_8Cz7BBgBwW6iUk8NHLAv8TbUIdYbHgJ7vd2xq17zJW6/exec',
   openaiModel: 'gpt-5.5-pro',
 };
